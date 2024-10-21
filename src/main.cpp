2942887023af41b0a52a6f5aeb89ba9b010fb4e7
devalexxx <46360108+alspin8@users.noreply.github.com>
2024-10-21 11:49:01 +0200

    Skydiver with Verlet integration

--- src/main.cpp
+++ src/main.cpp
@@ -25,10 +25,7 @@ int main(int /* argc */, char** /* argv */)
     float m = 80;
 
     glm::vec3 p0(0, 4000, 0);
-    glm::vec3 v0(200 / 3.6, 0, 0);
-
-    glm::vec3 fg = glm::vec3(0, -1, 0) * m * g;
-    glm::vec3 fd = glm::vec3(0, 0, 0);
+    glm::vec3 v0(50.f, 0, 0);
 
     //	float rho = 1.2;
     //	float A   = 1.9;
@@ -36,34 +33,43 @@ int main(int /* argc */, char** /* argv */)
     //  float coef = should be 0.5 * rho * A * Cd
     //  But work with fg / glm::length(glm::vec3(0, 50, 0)) ** 2)
     //	float coef = -.32f;
-    float coef = (fg / glm::pow(glm::length(glm::vec3(0, 50, 0)), 2.f)).y;
+    float coef = -(m * g) / (50.f * 50.f);
 
-    fmt::print("{}\n", coef);
+    glm::vec3 fg = glm::vec3(0, -1, 0) * m * g;
+    glm::vec3 fd = coef * glm::length(v0) * v0;
 
-    glm::vec3 p = p0;
-    glm::vec3 v = v0;
-    glm::vec3 a;
-
-    float t  = 0;
-    float dt = 0.01;
+    float dt = 0.3;
     float tt = 100;
+    float t  = 0;
 
     std::stringstream vStream;
     std::stringstream pStream;
     std::stringstream aStream;
 
+    glm::vec3 a   = (fg + fd) / m;
+    glm::vec3 pdt = p0;
+    glm::vec3 pt  = p0 + v0 * dt;
+    glm::vec3 p   = pt;
+    glm::vec3 v   = v0;
+
+    vStream << fmt::format("{:.2f} {}\n", t, v0);
+    pStream << fmt::format("{:.2f} {}\n", t, p0);
+    aStream << fmt::format("{:.2f} {}\n", t, a);
+
     while (t < tt)
     {
+        t += dt;
+
+        pt  = p;
+        p   = 2.f * pt - pdt + (dt * dt) * a;
+        pdt = pt;
+        v   = (p - pdt) / dt;
+
         if (float norm = glm::length(v); norm != 0)
             fd = coef * norm * v;
         else
             fd = glm::vec3(0, 0, 0);
-
-        p += dt * v;
         a = (fg + fd) / m;
-        v += dt * a;
-
-        t += dt;
 
         vStream << fmt::format("{:.2f} {}\n", t, v);
         pStream << fmt::format("{:.2f} {}\n", t, p);
@@ -98,7 +104,7 @@ int main(int /* argc */, char** /* argv */)
         std::ofstream aOut("aout.txt");
         if (aOut.is_open())
         {
-            aOut << vStream.str();
+            aOut << aStream.str();
         }
         else
         {
